bab4e9c31f2751646b8ac3615c6ddba025135b8e
Anjali <naik01anjali@gmail.com>
2025-12-07 22:58:08 +0530

    Update led_tasks.h

--- src/led_tasks.h
+++ src/led_tasks.h
@@ -1,1 +1,22 @@
+/**
+ * led_tasks.h
+ * -----------
+ * Declaration of FreeRTOS LED simulation tasks.
+ *
+ * In this repository, the LEDs are not physically driven by GPIO pins.
+ * Instead, the tasks log their activity to the console to illustrate
+ * real-time scheduling behavior in a hardware-agnostic way.
+ */
+
+#ifndef LED_TASKS_H
+#define LED_TASKS_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+// Task prototypes
+void fastBlinkTask(void *pvParameters);
+void slowBlinkTask(void *pvParameters);
+
+#endif // LED_TASKS_H
 
